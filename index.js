cb372c87b47371cb5af1fd7af452bbbca4f136dc
DenisLinenko <Denis.Linenko@outlook.com>
2019-12-29 17:57:24 -0500

    finished

--- index.js
+++ index.js
@@ -1,94 +1,90 @@
 var fs = require('fs')
 
 const delimiters = {
-	pipe: "|",
-	comma: ",",
-	space: " ",
+	pipe: {symbol: "|", last_name: 0, first_name: 1, gender: 3, date: 5, color: 4},
+	comma: {symbol: ",", last_name: 0, first_name: 1, gender: 2, date: 4, color: 3},
+	space: {symbol: " ", last_name: 0, first_name: 1, gender: 3, date: 4, color: 5},
 }
 
-const output = []
+let output = []
 const processFile = (fileName) => {
-	const delimeter = delimiters[fileName.split(".")[0]]
-	const lines = fs.readFileSync(fileName, 'utf8').split('\n').filter((item) => item.length > 0)
-	
+
 	const genderConverter = (data) => {
-		data[2] = data[2] === "M" ? "Male" : "Female"
+		let result
+		if(data === "M") {
+			result = "Male"
+		} else if(data === "F") {
+			result = "Female"
+		} else {
+			result = data
+		}
+		return result
 	}
 
-	lines.forEach((line) => {
-		const splitedLine = line.split(delimeter)
-		const player = splitedLine.map((item) =>	item.trim())
-
-		if(delimeter === "|") {
-			player.splice(2,1)
-			const color = player[3]
-			player[3] = new Date(player[4]).toLocaleDateString()
-			player[4] = color
-			genderConverter(player)
-			output.push(player)
-		
-		} if(delimeter === ",") {
-			const color = player[3]
-			player[3] = new Date(player[4]).toLocaleDateString()
-			player[4] = color
-			output.push(player)
-
-		} if(delimeter === " ") {
-				player.splice(2,1)
-				player[3] = new Date(player[3]).toLocaleDateString()
-				genderConverter(player)
-				output.push(player)
-			} 
-	})
+	const delimeter = delimiters[fileName.split(".")[0]]
+	if(delimeter){
+		const lines = fs.readFileSync(fileName, 'utf8').split('\n')
+	
+		lines.forEach((line) => {
+			if(line.trim().length > 0) {
+				const splitedLine = line.split(delimeter.symbol)
+				const player = splitedLine.map((item) => item.trim())
+				
+				let result = {
+					last_name: player[delimeter.last_name],
+					first_name: player[delimeter.first_name],
+					color: player[delimeter.color],
+					date: new Date(player[delimeter.date]).getTime(),
+					gender: genderConverter(player[delimeter.gender])
+				}
+				output.push(result)
+			}
+		})
+	}
+	
 }
 processFile("pipe.txt")
 processFile("comma.txt")
 processFile("space.txt")
 
-const print = (name, data) => {
-	console.log(name + ":")
-	data.forEach((item) => {
-		let row = ''
-		item.forEach((word) => {
-			row += " " + word
-		})
+const print = (name, items) => {
+	console.log(`\n${name} : `)
+	items.forEach(({last_name, first_name, gender, date, color}) => {
+		let row = `${last_name} ${first_name} ${gender} ${new Date(date).toLocaleDateString()} ${color}`
 		console.log(row)
 	})
 }
-
 const data1 = output.sort((a, b) => {
-	return a[2] === b[2] ? (a[0] > b[0] ? 1 : -1) : a[2] > b[2] ? 1 : -1
-	// if(a[2] === b[2]) {
-	// 	if(a[0] > b[0]) {
-	// 		 return 1
-	// 	 } else {
-	// 		 return -1
-	// 	 }
-	//  } else if(a[2] > b[2]) {
-	// 	 return 1
-	//  } else {
-	// 	 return -1
-	//  }
+	if(a.gender === b.gender) {
+		if(a.last_name > b.last_name) {
+			 return 1
+		 } else {
+			 return -1
+		 }
+	 } else if(a.gender > b.gender) {
+		 return 1
+	 } else {
+		 return -1
+	 }
 })
 print("Output 1", data1)
 
 const data2 = output.sort((a, b) => {
-	return new Date(a[3]).getTime() === new Date(b[3]).getTime() ? (a[0] > b[0] ? 1 : -1) : new Date(a[3]).getTime() > new Date(b[3]).getTime() ? 1 : -1
-	// if(new Date(a[3]).getTime() === new Date(b[3]).getTime()) {
-	//  if(a[0] > b[0]) {
-	// 		return 1
-	// 	} else {
-	// 		return -1
-	// 	}
-	// } else if(new Date(a[3]).getTime() > new Date(b[3]).getTime()) {
-	// 	return 1
-	// } else {
-	// 	return -1
-	// }
+	if(a.date === b.date) {
+	 if(a.last_name > b.last_name) {
+			return 1
+		} else {
+			return -1
+		}
+	} else if(a.date > b.date) {
+		return 1
+	} else {
+		return -1
+	}
 })
 print("Output 2", data2)
 
 const data3 = output.sort((a, b) => {
-	return a[0] < b[0] ? 1 : -1
+	return a.last_name < b.last_name ? 1 : -1
 })
 print("Output 3", data3)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Defmethod
+
+## Requirements
+
+3 files must be present in the same folder pipe.txt, space.txt and comma.txt
+
+## To run
+
+`node index.js` to run application
+
+## Support 
+
+If you have any questions please let me know. Denis Linenko linenkonyc@gmail.com
